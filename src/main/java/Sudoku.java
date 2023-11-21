82e0b207c149b3f3239adadd9ee12bee3e566c5e
blesseditsme <yan.tsyvunchyk@gmail.com>
2023-11-21 14:24:01 -0500

    V.1

---
+++ src/main/java/Sudoku.java
@@ -0,0 +1,93 @@
+public class Sudoku {
+    public static void main(String[] args) {
+        int[][] board = {
+                {5, 3, 0, 0, 7, 0, 0, 0, 0},
+                {6, 0, 0, 1, 9, 5, 0, 0, 0},
+                {0, 9, 8, 0, 0, 0, 0, 6, 0},
+                {8, 0, 0, 0, 6, 0, 0, 0, 3},
+                {4, 0, 0, 8, 0, 3, 0, 0, 1},
+                {7, 0, 0, 0, 2, 0, 0, 0, 6},
+                {0, 6, 0, 0, 0, 0, 2, 8, 0},
+                {0, 0, 0, 4, 1, 9, 0, 0, 5},
+                {0, 0, 0, 0, 8, 0, 0, 7, 9}
+        };
+
+        if (solveSudoku(board)) {
+            printBoard(board);
+        } else {
+            System.out.println("No solution exists.");
+        }
+    }
+
+    private static boolean solveSudoku(int[][] board) {
+        int[] emptyCell = findEmptyCell(board);
+
+        if (emptyCell == null) {
+            // No empty cells left, puzzle is solved
+            return true;
+        }
+
+        int row = emptyCell[0];
+        int col = emptyCell[1];
+
+        for (int num = 1; num <= 9; num++) {
+            if (isValid(board, row, col, num)) {
+                // Try placing the number in the cell
+                board[row][col] = num;
+
+                // Recursively solve the rest of the puzzle
+                if (solveSudoku(board)) {
+                    return true; // Puzzle is solved
+                }
+
+                // If placing the number doesn't lead to a solution, backtrack
+                board[row][col] = 0;
+            }
+        }
+
+        // No valid number can be placed in the current cell
+        return false;
+    }
+
+    private static boolean isValid(int[][] board, int row, int col, int num) {
+        // Check if the number is already present in the row or column
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == num || board[i][col] == num) {
+                return false;
+            }
+        }
+
+        // Check if the number is present in the 3x3 grid
+        int startRow = 3 * (row / 3);
+        int startCol = 3 * (col / 3);
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[startRow + i][startCol + j] == num) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private static int[] findEmptyCell(int[][] board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == 0) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null; // No empty cells left
+    }
+
+    private static void printBoard(int[][] board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
